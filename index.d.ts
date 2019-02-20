ccfaa243d28279eeb659a5aec8d8253746760abf
Alexander Cerutti <cerutti.alexander@gmail.com>
2019-02-20 20:20:23 +0100

    Improved typescript typings

--- index.d.ts
+++ index.d.ts
@@ -1,14 +1,22 @@
-import { PartialObserver } from "rxjs";
+import { PartialObserver, Subject } from "rxjs";
 
 declare module "proxy-observer" {
 	class ObservableObject<T> {
-		new (from: T): ObservableObject<T> & ProxyConstructor;
-		observe(): SubjectLike;
-		unsubscribeAll(): void;
+		private _observedObjects: Observed;
+		new (from: T): T & ObservableObject<T>;
+		observe?(): SubjectLike;
+		unsubscribeAll?(): void;
 	}
 
 	interface SubjectLike {
 		subscribe(observer: PartialObserver<any>): void;
 		unsubscribe(): void;
+	}
+
+	interface Observed {
+		[key: string]: {
+			count: number,
+			ref: Subject<any>
+		}
 	}
 }
