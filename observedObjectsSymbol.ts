a0f5838161d2aeeeaaf6c98cc38f3613696baa3b
Alexander Cerutti <cerutti.alexander@gmail.com>
2019-03-08 13:08:42 +0100

    Added comment to observedObjectsSymbol

--- observedObjectsSymbol.ts
+++ observedObjectsSymbol.ts
@@ -1,1 +1,8 @@
+/**
+ * I had to create a whole new file for this to export
+ * the symbol both to the main file and the tests file.
+ * Otherwise, I wouldn't have been able to access to
+ * structures.
+ */
+
 export const observedObjects = Symbol("_observedObjects");
