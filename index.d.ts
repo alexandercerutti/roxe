715d1d3e276a0f8de4794f61ada833183be25a63
Alexander Cerutti <cerutti.alexander@gmail.com>
2019-05-01 01:16:16 +0200

    Updated typescript declarations

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
-import { Subject } from "rxjs";
+import { Subject, Subscription } from "rxjs";
 
 declare module "rexursive-observer" {
 	const ObservableObject: ObservableConstructor;
@@ -11,7 +11,32 @@ declare module "rexursive-observer" {
 
 declare class _ObservableObject<T> {
 	new (from: T): T & _ObservableObject<T>;
-	observe<A = any>(): Subject<A>;
-	unsubscribeAll(): void;
-	snapshot(): T;
+
+	/**
+	 * Registers a custom property to be observed.
+	 *
+	 * @param {string} prop - The property or object
+	 *    property to subscribe to (e.g. `epsilon`
+	 *    or `time.current`)
+	 */
+
+	observe<A = any>(prop: string): Subject<A>;
+
+	/**
+	 * Unsubscribes from all the subscriptions in a specific pool
+	 * @param subscriptions
+	 */
+
+	unsubscribeAll(subscriptions: Subscription[]): void;
+
+	/**
+	 * Returns the current image of a key of the main
+	 * object or a nested key.
+	 *
+	 * @param {string} path - dotted-notation path ("a.b.c")
+	 * @returns {any} - the whole observed object or part of it
+	 * @throws if the current path does not reflect to an available object
+	 */
+
+	snapshot(path?: string): T;
 }
